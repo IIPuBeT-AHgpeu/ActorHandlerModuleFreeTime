015262827e444f854f435ad030b31940feaa4aae
kiran2000 <kiran2000@yandex.ru>
2021-06-12 14:19:27 +0300

    Добавил отладочные комментарии к методду pathfinding.

--- ActorHandlerModuleFreeTime/MovementActivityFreeTime.cs
+++ ActorHandlerModuleFreeTime/MovementActivityFreeTime.cs
@@ -113,84 +113,96 @@ namespace ActorHandlerModuleFreeTime
 #if DEBUG
             Console.WriteLine("Actor is moving (freetime).");
 #endif
-            if(!IsHaveDestination)
+            if (!IsHaveDestination)
             {
                 Destination = actor.GetState<PlaceState>().FavoritePlaces[ChoosePlace(actor)];
                 IsHaveDestination = true;
             }
-            // Расстояние, которое может пройти актор с заданной скоростью за прошедшее время
-            double distance = actor.GetState<SpecState>().Speed * deltaTime;
-
-            SecondsToUpdate += deltaTime;
-
-            if (SecondsToUpdate >= 1)
-            {
-                // Уменьшаем параметр голода, усталости, настроения
-                if (actor.GetState<SpecState>().Satiety <= 0.001 * 100) actor.GetState<SpecState>().Satiety = 0;
-                else actor.GetState<SpecState>().Satiety -= 0.001 * 100;
-
-                if (actor.GetState<SpecState>().Stamina <= 0.001 * 100) actor.GetState<SpecState>().Stamina = 0;
-                else actor.GetState<SpecState>().Stamina -= 0.001 * 100;
-
-                SecondsToUpdate -= 1;
-            }
-            // Определяем текущий приоритет для активностей FreeTime
-            if (actor.GetState<SpecState>().Mood <= (0.05 * 100)) Priority = 92;
-            else if (actor.GetState<SpecState>().Mood > (0.05 * 100) && actor.GetState<SpecState>().Mood <= (0.1 * 100)) Priority = 82;
-            else if (actor.GetState<SpecState>().Mood > (0.1 * 100) && actor.GetState<SpecState>().Mood <= (0.3 * 100)) Priority = 62;
-            else if (actor.GetState<SpecState>().Mood > (0.3 * 100) && actor.GetState<SpecState>().Mood <= (0.6 * 100)) Priority = 42;
-            else if (actor.GetState<SpecState>().Mood > (0.6 * 100) && actor.GetState<SpecState>().Mood <= (0.8 * 100)) Priority = 22;
-            else if (actor.GetState<SpecState>().Mood > 0.8 * 100) Priority = 2;
-#if DEBUG
-            Console.WriteLine($"Hunger: {actor.GetState<SpecState>().Satiety}; Mood: {actor.GetState<SpecState>().Mood}; Fatigue: {actor.GetState<SpecState>().Stamina}");
-#endif
-            if (IsPath)
-            {
-                var firstCoordinate = new Coordinate(actor.X, actor.Y);
-                var secondCoordinate = new Coordinate(Destination.X, Destination.Y);
-
-                Path = PathsFinding.GetPath(firstCoordinate, secondCoordinate, "Walking").Result.Coordinates;
-                IsPath = false;
-            }
-
-            Vector2D direction = new Vector2D(actor.Coordinate, Path[i]);
-            // Проверка на перешагивание
-
-            if (direction.Length() <= distance)
-            {
-                // Шагаем в точку, если она ближе, чем расстояние которое можно пройти
-                actor.X = Path[i].X;
-                actor.Y = Path[i].Y;
-            }
             else
             {
-                // Вычисляем новый вектор, с направлением к точке назначения и длинной в distance
-                direction = direction.Normalize().Multiply(distance);
+                // Расстояние, которое может пройти актор с заданной скоростью за прошедшее время
+                double distance = actor.GetState<SpecState>().Speed * deltaTime;
 
-                // Смещаемся по вектору
-                actor.X += direction.X;
-                actor.Y += direction.Y;
-            }
+                SecondsToUpdate += deltaTime;
 
-            if (actor.X == Path[i].X && actor.Y == Path[i].Y && i < Path.Length - 1)
-            {
-                i++;
+                if (SecondsToUpdate >= 1)
+                {
+                    // Уменьшаем параметр голода, усталости, настроения
+                    if (actor.GetState<SpecState>().Satiety <= 0.001 * 100) actor.GetState<SpecState>().Satiety = 0;
+                    else actor.GetState<SpecState>().Satiety -= 0.001 * 100;
+
+                    if (actor.GetState<SpecState>().Stamina <= 0.001 * 100) actor.GetState<SpecState>().Stamina = 0;
+                    else actor.GetState<SpecState>().Stamina -= 0.001 * 100;
+
+                    SecondsToUpdate -= 1;
+                }
+                // Определяем текущий приоритет для активностей FreeTime
+                if (actor.GetState<SpecState>().Mood <= (0.05 * 100)) Priority = 92;
+                else if (actor.GetState<SpecState>().Mood > (0.05 * 100) && actor.GetState<SpecState>().Mood <= (0.1 * 100)) Priority = 82;
+                else if (actor.GetState<SpecState>().Mood > (0.1 * 100) && actor.GetState<SpecState>().Mood <= (0.3 * 100)) Priority = 62;
+                else if (actor.GetState<SpecState>().Mood > (0.3 * 100) && actor.GetState<SpecState>().Mood <= (0.6 * 100)) Priority = 42;
+                else if (actor.GetState<SpecState>().Mood > (0.6 * 100) && actor.GetState<SpecState>().Mood <= (0.8 * 100)) Priority = 22;
+                else if (actor.GetState<SpecState>().Mood > 0.8 * 100) Priority = 2;
 #if DEBUG
-                Console.WriteLine(i);
-                Console.WriteLine(Path.Length);
+                Console.WriteLine($"Hunger: {actor.GetState<SpecState>().Satiety}; Mood: {actor.GetState<SpecState>().Mood}; Fatigue: {actor.GetState<SpecState>().Stamina}");
 #endif
-            }
+                if (IsPath)
+                {
+                    var firstCoordinate = new Coordinate(actor.X, actor.Y);
+                    var secondCoordinate = new Coordinate(Destination.X, Destination.Y);
+#if DEBUG
+                    Console.WriteLine("Start PathsFinding...");
+#endif
+                    Path = PathsFinding.GetPath(firstCoordinate, secondCoordinate, "Walking").Result.Coordinates;
+                    IsPath = false;
+#if DEBUG
+                    if (Path == null)
+                        Console.WriteLine("Have no Path.");
+                    else
+                        Console.WriteLine("Have Path");
+                    Console.WriteLine("Finish PathsFinding.");
+#endif
+                }
 
-            // Если в процессе шагания мы достигли точки назначения
-            if (actor.X == Path[Path.Length - 1].X && actor.Y == Path[Path.Length - 1].Y)
-            {
-                Console.WriteLine("Start WaitingFreeTime");
-                i = 0;
-                IsPath = true;
-                actor.Activity = new WaitingActivityFreeTime(Priority, Destination.TagKey, SecondsToUpdate);
-                IsHaveDestination = false;
-                Priority = 0;
-                //return true;
+                Vector2D direction = new Vector2D(actor.Coordinate, Path[i]);
+                // Проверка на перешагивание
+
+                if (direction.Length() <= distance)
+                {
+                    // Шагаем в точку, если она ближе, чем расстояние которое можно пройти
+                    actor.X = Path[i].X;
+                    actor.Y = Path[i].Y;
+                }
+                else
+                {
+                    // Вычисляем новый вектор, с направлением к точке назначения и длинной в distance
+                    direction = direction.Normalize().Multiply(distance);
+
+                    // Смещаемся по вектору
+                    actor.X += direction.X;
+                    actor.Y += direction.Y;
+                }
+
+                if (actor.X == Path[i].X && actor.Y == Path[i].Y && i < Path.Length - 1)
+                {
+                    i++;
+#if DEBUG
+                    Console.WriteLine(i);
+                    Console.WriteLine(Path.Length);
+#endif
+                }
+
+                // Если в процессе шагания мы достигли точки назначения
+                if (actor.X == Path[Path.Length - 1].X && actor.Y == Path[Path.Length - 1].Y)
+                {
+                    Console.WriteLine("Start WaitingFreeTime");
+                    i = 0;
+                    IsPath = true;
+                    actor.Activity = new WaitingActivityFreeTime(Priority, Destination.TagKey, SecondsToUpdate);
+                    IsHaveDestination = false;
+                    Priority = 0;
+                    //return true;
+                }
             }
             return false;
         }
